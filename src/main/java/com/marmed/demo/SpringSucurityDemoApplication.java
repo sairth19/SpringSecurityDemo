c170490587e2fa90fa6cd4b65fb6053d134004dc
Sair Martinez <smartinezm@psl.com.co>
2019-11-08 15:05:23 -0500

    Initial Commit

---
+++ src/main/java/com/marmed/demo/SpringSucurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.marmed.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSucurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSucurityDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marmed/demo/SpringSucurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marmed.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSucurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
